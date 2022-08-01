5cfd3b9716e761c2a5216a488bd8a7ac127abb32
Andrei_Nadezhkin <Andreikoman@gmail.com>
2022-08-01 17:30:54 +0300

    added test.php

---
+++ web/test.php
@@ -0,0 +1,14 @@
+<?php
+//$handle_new = fopen("sites/default/files/users_ids.csv",'a+');
+//$handle_old = fopen("sites/default/files/users_0.csv",'r');
+//$id = 1;
+//while (($data = fgetcsv($handle_old)) !== false) {
+
+//  array_unshift($data, $id);
+//  $id++;
+
+//  fputcsv($handle_new, $data);
+//}
+//
+//fclose($handle_new);
+//fclose($handle_old);
