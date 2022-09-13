f0f1045d64d277180b5903ab34fbebdc384809bb
Andrei_Nadezhkin <Andreikoman@gmail.com>
2022-09-13 11:29:20 +0300

    added custom page with cached user

---
+++ web/modules/custom/an_task_78/src/Controller/TestCachePageController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\an_task_78\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class TestCachePageController
+ *
+ * @package Drupal\an_task_78\Controller
+ */
+class TestCachePageController extends ControllerBase{
+
+  /**
+   * @return \Drupal\Component\Render\MarkupInterface|string
+   */
+  public function getCachedUser() {
+    $cid = 'current_user';
+    $user = \Drupal::currentUser()->getDisplayName();
+    $cache = \Drupal::cache()->get($cid);
+
+    if ($cache && $user === $cache->data) {
+      $user = $cache->data;
+    } else {
+      \Drupal::cache()->set($cid, $user);
+    }
+
+    return $user;
+  }
+
+  /**
+   * @return array
+   */
+  public function content() {
+
+    return [
+      '#theme' => 'testcache_page',
+      '#current_user' => $this->getCachedUser(),
+    ];
+  }
+}
+
