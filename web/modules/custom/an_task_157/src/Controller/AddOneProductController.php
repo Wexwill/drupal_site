b26919484e459d6694b4b4ea4a133d95aa29d7f3
Andrei_Nadezhkin <Andreikoman@gmail.com>
2022-11-01 15:57:57 +0300

    Fixed code style

--- web/modules/custom/an_task_157/src/Controller/AddOneProductController.php
+++ web/modules/custom/an_task_157/src/Controller/AddOneProductController.php
@@ -2,22 +2,68 @@
 
 namespace Drupal\an_task_157\Controller;
 
+use Drupal\commerce_cart\CartManagerInterface;
+use Drupal\commerce_cart\CartProviderInterface;
 use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\AlertCommand;
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Class AddOneProductController
+ * Class AddOneProductController.
  *
  * @package Drupal\an_task_157\Controller
  */
 class AddOneProductController extends ControllerBase {
 
   /**
-   * @param $pid
-   * @param $quantity
+   * The cart manager.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected $cartManager;
+
+  /**
+   * The cart provider.
+   *
+   * @var \Drupal\commerce_cart\CartProviderInterface
+   */
+  protected $cartProvider;
+
+  /**
+   * AddOneProductController constructor.
+   *
+   * @param \Drupal\commerce_cart\CartManagerInterface $cart_manager
+   *   The cart provider.
+   * @param \Drupal\commerce_cart\CartProviderInterface $cart_provider
+   *   The cart provider.
+   */
+  public function __construct(CartManagerInterface $cart_manager, CartProviderInterface $cart_provider) {
+    $this->cartManager = $cart_manager;
+    $this->cartProvider = $cart_provider;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('commerce_cart.cart_manager'),
+      $container->get('commerce_cart.cart_provider')
+    );
+  }
+
+  /**
+   * Provides ability to add shareable product to the cart.
+   *
+   * @param int $pid
+   *   Product id.
+   * @param int $quantity
+   *   Amount of products.
    *
    * @return \Drupal\Core\Ajax\AjaxResponse
+   *   The ajax response.
+   *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    * @throws \Drupal\Core\Entity\EntityStorageException
@@ -30,33 +76,31 @@ class AddOneProductController extends ControllerBase {
       $store_id = 1;
       $order_type = 'default';
 
-      $entity_manager = \Drupal::entityTypeManager();
-      $cart_manager = \Drupal::service('commerce_cart.cart_manager');
-      $cart_provider = \Drupal::service('commerce_cart.cart_provider');
-      $store = $entity_manager->getStorage('commerce_store')->load($store_id);
+      $store = $this->entityTypeManager()->getStorage('commerce_store')->load($store_id);
 
       // Load current users cart.
-      $cart = $cart_provider->getCart($order_type, $store);
+      $cart = $this->cartProvider->getCart($order_type, $store);
       if (!$cart) {
-        $cart = $cart_provider->createCart($order_type, $store);
+        $cart = $this->cartProvider->createCart($order_type, $store);
       }
 
-      $product_variation = $entity_manager->getStorage('commerce_product_variation')->load($pid);
+      $product_variation = $this->entityTypeManager()->getStorage('commerce_product_variation')->load($pid);
 
       // Create new order item.
-      $order_item = $entity_manager->getStorage('commerce_order_item')->create(array(
+      $order_item = $this->entityTypeManager()->getStorage('commerce_order_item')->create([
         'type' => 'default',
         'purchased_entity' => (string) $pid,
         'quantity' => $quantity,
         'unit_price' => $product_variation->getPrice(),
-      ));
+      ]);
       $order_item->save();
 
-      $cart_manager->addOrderItem($cart, $order_item);
+      $this->cartManager->addOrderItem($cart, $order_item);
 
       $ajax_response->addCommand(new AlertCommand('The product has been added to your cart!'));
     }
 
     return $ajax_response;
   }
+
 }
--- web/modules/custom/an_task_157/src/Form/ShareableCartForm.php
+++ web/modules/custom/an_task_157/src/Form/ShareableCartForm.php
@@ -2,9 +2,13 @@
 
 namespace Drupal\an_task_157\Form;
 
+use Drupal\commerce_cart\CartManagerInterface;
+use Drupal\commerce_cart\CartProviderInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Implements Form API.
@@ -12,11 +16,63 @@ use Drupal\Core\Url;
 class ShareableCartForm extends FormBase {
 
   /**
+   * Contains shopping cart data.
+   *
    * @var array
    */
-  private $cart_data;
+  private $cartData;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The cart manager.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected $cartManager;
+
+  /**
+   * The cart provider.
+   *
+   * @var \Drupal\commerce_cart\CartProviderInterface
+   */
+  protected $cartProvider;
+
+  /**
+   * AddOneProductController constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\commerce_cart\CartManagerInterface $cart_manager
+   *   The cart provider.
+   * @param \Drupal\commerce_cart\CartProviderInterface $cart_provider
+   *   The cart provider.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager, CartManagerInterface $cart_manager, CartProviderInterface $cart_provider) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->cartManager = $cart_manager;
+    $this->cartProvider = $cart_provider;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager'),
+      $container->get('commerce_cart.cart_manager'),
+      $container->get('commerce_cart.cart_provider')
+    );
+  }
+
+  /**
+   * Form id.
+   *
    * @inheritDoc
    */
   public function getFormId() {
@@ -24,6 +80,8 @@ class ShareableCartForm extends FormBase {
   }
 
   /**
+   * Form constructor.
+   *
    * @inheritDoc
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
@@ -39,14 +97,15 @@ class ShareableCartForm extends FormBase {
     if (!empty($cart_data)) {
       $header = ['Product', 'Price', 'Quantity', 'Total price'];
 
-      $cart_data = json_decode($cart_data, true);
-      $this->cart_data = $cart_data;
+      $cart_data = json_decode($cart_data, TRUE);
+      $this->cartData = $cart_data;
 
       foreach ($cart_data as $key => $product) {
         $sku = array_pop($product);
 
-        $link = Url::fromRoute('an_task_157.add_one_product', ['pid' => $sku, 'quantity' => $product['quantity']])
-                ->toString();
+        $link = Url::fromRoute('an_task_157.add_one_product',
+                ['pid' => $sku, 'quantity' => $product['quantity']])
+          ->toString();
 
         $product[] = [
           'data' => [
@@ -78,34 +137,33 @@ class ShareableCartForm extends FormBase {
   }
 
   /**
+   * Form submission handler.
+   *
    * @inheritDoc
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $cart_data = $this->cart_data;
+    $cart_data = $this->cartData;
 
     if (!empty($cart_data)) {
       $store_id = 1;
       $order_type = 'default';
 
-      $entity_manager = \Drupal::entityTypeManager();
-      $cart_manager = \Drupal::service('commerce_cart.cart_manager');
-      $cart_provider = \Drupal::service('commerce_cart.cart_provider');
-      $store = $entity_manager->getStorage('commerce_store')->load($store_id);
+      $store = $this->entityTypeManager->getStorage('commerce_store')->load($store_id);
 
       // Load current users cart.
-      $cart = $cart_provider->getCart($order_type, $store);
+      $cart = $this->cartProvider->getCart($order_type, $store);
 
       if (!$cart) {
-        $cart = $cart_provider->createCart($order_type, $store);
+        $cart = $this->cartProvider->createCart($order_type, $store);
       }
 
       // Push each product to the users cart.
       foreach ($cart_data as $product) {
-        $product_variation = $entity_manager->getStorage('commerce_product_variation')
+        $product_variation = $this->entityTypeManager->getStorage('commerce_product_variation')
           ->load($product['sku']);
 
         // Create new order item.
-        $order_item = $entity_manager->getStorage('commerce_order_item')->create([
+        $order_item = $this->entityTypeManager->getStorage('commerce_order_item')->create([
           'type' => 'default',
           'purchased_entity' => (string) $product['sku'],
           'quantity' => $product['quantity'],
@@ -113,10 +171,11 @@ class ShareableCartForm extends FormBase {
         ]);
         $order_item->save();
 
-        $cart_manager->addOrderItem($cart, $order_item);
+        $this->cartManager->addOrderItem($cart, $order_item);
       }
 
       $form_state->setRedirect('commerce_cart.page');
     }
   }
+
 }
