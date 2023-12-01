bc742bf712812c090f7593bd576e2452b1003b79
Andrei_Nadezhkin <Andreikoman@gmail.com>
2023-12-01 13:26:18 +0300

    removed module

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '875fa02c8857ac889c386a27464b566e58438322',
+        'reference' => '6b68d0eb318386e22746eef746bbef630155e879',
         'name' => 'drupal/recommended-project',
         'dev' => true,
     ),
@@ -1008,15 +1008,6 @@
                 0 => '9.4.2',
             ),
         ),
-        'drupal/permissions_by_term' => array(
-            'pretty_version' => '3.1.29',
-            'version' => '3.1.29.0',
-            'type' => 'drupal-module',
-            'install_path' => __DIR__ . '/../../web/modules/contrib/permissions_by_term',
-            'aliases' => array(),
-            'reference' => '3.1.29',
-            'dev_requirement' => false,
-        ),
         'drupal/pgsql' => array(
             'dev_requirement' => false,
             'replaced' => array(
@@ -1059,7 +1050,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '875fa02c8857ac889c386a27464b566e58438322',
+            'reference' => '6b68d0eb318386e22746eef746bbef630155e879',
             'dev_requirement' => false,
         ),
         'drupal/responsive_image' => array(
